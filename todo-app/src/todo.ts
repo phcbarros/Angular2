acc71354c8078a9c2e9b75a3f2eafd1ed332526b
Paulo Henrique da Costa Barros <paulo_costinha@hotmail.com>
2016-01-17 15:23:35 -0200

    Todo App com Angular 2

---
+++ todo-app/src/todo.ts
@@ -0,0 +1,5 @@
+// Declara an interface for type safety
+export interface Todo{
+    text: string,
+    done: boolean
+}
---
+++ todo-app/src/todo_list.ts
@@ -0,0 +1,24 @@
+import {Component, Input} from 'angular2/core';
+import {Todo} from "./todo";
+
+@Component({
+    selector: 'todo-list',
+    styles: [`
+        .done-true{
+            text-decoration: line-through;
+            color: true;
+        }
+    `],
+    template: `
+        <ul class="unstyled">
+            <li *ngFor="#todo of todos">
+                <input type="checkbox" [(ngModel)]="todo.done">
+                <span class="done-{{todo.done}}">{{todo.text}}</span>
+            </li>
+        </ul>
+    `
+})
+
+export class TodoList {
+   @Input() todos: Todo[]; 
+}
