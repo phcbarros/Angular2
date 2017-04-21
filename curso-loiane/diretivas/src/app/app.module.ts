29c8c8ac2d12d82f2819b7c6cb3f07b3da6aa4b8
Paulo Barros <paulohcbarros@outlook.com>
2017-04-21 14:10:27 -0300

    Curso Loiane - 27 - Diretivas: ngFor

--- curso-loiane/diretivas/src/app/app.module.ts
+++ curso-loiane/diretivas/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { DiretivaNgIfComponent } from './diretiva-ng-if/diretiva-ng-if.component';
 import { DiretivaNgSwitchComponent } from './diretiva-ng-switch/diretiva-ng-switch.component';
+import { DiretivaNgForComponent } from './diretiva-ng-for/diretiva-ng-for.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DiretivaNgIfComponent,
-    DiretivaNgSwitchComponent
+    DiretivaNgSwitchComponent,
+    DiretivaNgForComponent
   ],
   imports: [
     BrowserModule,
