ae055dcd880a27b949c9b2b1bbe1c8ae7cfdaeec
Paulo Barros <paulohcbarros@outlook.com>
2017-06-11 11:00:59 -0300

    Curso Loiane - 46 - Pipes: Criando um Pipe "Puro"

--- curso-loiane/pipes/src/app/app.module.ts
+++ curso-loiane/pipes/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppComponent } from './app.component';
 import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
 import { CamelCasePipe } from './camel-case.pipe';
 import { SettingsService } from './settings.service';
+import { FiltroArrayPipe } from './filtro-array.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     ExemplosPipesComponent,
-    CamelCasePipe
+    CamelCasePipe,
+    FiltroArrayPipe
   ],
   imports: [
     BrowserModule,
@@ -20,16 +22,16 @@ import { SettingsService } from './settings.service';
     HttpModule
   ],
   providers: [
-    /*{
-      provide: LOCALE_ID,
-      useValue: 'pt-BR'
-    }*/
-    SettingsService,
     {
       provide: LOCALE_ID,
-      deps: [SettingsService],
-      useFactory: settingsService => settingsService.getLocale()
+      useValue: 'pt-BR'
     }
+    // SettingsService,
+    // {
+    //   provide: LOCALE_ID,
+    //   deps: [SettingsService],
+    //   useFactory: settingsService => settingsService.getLocale()
+    // }
   ],
   bootstrap: [AppComponent]
 })
--- curso-loiane/pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
+++ curso-loiane/pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -17,7 +17,14 @@ export class ExemplosPipesComponent implements OnInit {
     descricao: 'Influenciado por obras “cyberpunk” do final dos anos 1980 como Akira e por filmes como Blade Runner - O Caçador de Androides, o cenário escolhido por Masamune Shirow para The Ghost in the Shell foi o futuro distópico de 2029, onde a alta tecnologia se mistura a uma sociedade decadente e desigual.É nesse mundo à beira do colapso que a Major Motoko Kusanagi encabeça a Seção 9 da Segurança Pública japonesa. Motoko é uma ciborgue altamente treinada incumbida de desmantelar uma série de crimes cibernéticos realizados por um hacker conhecido como o Mestre dos Fantoches. Em meio à caça ao criminoso virtual, Masamune Shirow insere na trama questionamentos existencialistas, ponderando até mesmo se alguém provido meramente de Inteligência Artificial é, de fato, um ser vivo. E foi exatamente essa mistura de ficção científica, ação e temas filosóficos que fizeram do mangá The Ghost in the Shell uma leitura obrigatória.'
   };
 
+  livros: string[] = ['Java', 'Angular'];
+
   ngOnInit() {
+  }
+
+  addLivro(livro:string){
+    this.livros.push(livro);
+    console.log(this.livros);
   }
 
 }
